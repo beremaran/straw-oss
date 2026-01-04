67628380dc7e05ae6e8d546cd347f7292a2a496a
Ryan Arslan <ryan@beremaran.com>
2026-01-04 16:54:18 +0800

    feat: Replace `key_hash` with `token_hash` in API documentation, add UUIDs to seeded data, and include a token test script.

---
+++ scripts/check-ip.sh
@@ -0,0 +1,6 @@
+curl -X 'POST' \
+  'http://localhost:8080/v2/request' \
+  -H 'accept: application/json' \
+  -H 'Authorization: Bearer dev-test-token-12345' \
+  -H 'Content-Type: application/json' \
+  -d '{"url": "https://httpbin.org/ip"}'
