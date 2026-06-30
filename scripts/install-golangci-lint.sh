7f86af4e9017c977a6ba9b1898bad14ce9ff5726
Berke Arslan <berke@kwilabs.com>
2026-06-30 17:23:40 +0800

    reset

--- scripts/install-golangci-lint.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-if [[ "$OSTYPE" == "darwin"* ]]; then
-    brew install golangci-lint
-else
-    curl -sSfL https://golangci-lint.run/install.sh | sh -s -- -b $(go env GOPATH)/bin v2.12.2
-fi
